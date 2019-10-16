f13220250c833cea0bc065900ab811dbc48e9e85
Raphaël Viudès <raphael.viudes@ecl19.ec-lyon.fr>
2019-10-16 15:22:34 +0200

    Ajout pages écran + création programme affichage

---
+++ EL_Electrical/Dashboard controller/Nextion and Teensy Interface/nextion_communication/nextion_communication.ino
@@ -0,0 +1,32 @@
+#include <string.h>
+
+char id="t3";
+char texte="Bonjour";
+void setup(){
+  Serial.begin(9600);
+}
+
+void Nextion_EndMessage(){
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+}
+
+void Nextion_SendText(char txtid,char txt){
+  Serial.print(txtid+".txt=");
+  Serial.print("\"");
+  Serial.print(txt);
+  Serial.print("\"");
+  Nextion_EndMessage();
+}
+
+void Nextion_SendVal(char valid,float val){
+  Serial.print(valid+".val=");
+  Serial.print(val);
+  Nextion_EndMessage();
+}
+
+void loop(){
+  Nextion_SendText(id,texte);
+}
+
