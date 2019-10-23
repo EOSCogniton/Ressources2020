d355517a37eb3c428d19d934136978201fe23320
Raphaël Viudès <raphael.viudes@ecl19.ec-lyon.fr>
2019-10-23 17:16:32 +0200

    Création fichiers h et cpp Nextion et Shiftlight + PinMap

---
+++ EL_Electrical/Dashboard controller/Arduino Invictus/front card/nextion_communication.h
@@ -0,0 +1,33 @@
+#ifndef nextion_communication
+#define nextion_communication
+
+
+
+/**************************************************************************/
+//    Inclusion of the necessary libraries
+/**************************************************************************/
+#include "shiftlight.h"
+
+
+/**************************************************************************/
+//    Functions
+/**************************************************************************/
+void changePage(int page);
+
+void startDisplay(int blueShiftPin,int redShiftPin);
+
+void updateDisplay(int page,char gear,int oilTemp, float voltage,int rpm, bool launch);
+
+void setGear(char gear);
+
+void setOilTemp(int oilTemp);
+
+void setVoltage(int voltage);
+
+void setRPM(int RPM);
+
+void setLaunch(bool launch_active);
+
+void nextion_endMessage();
+
+#endif
