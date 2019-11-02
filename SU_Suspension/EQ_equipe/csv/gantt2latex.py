bc5d6270c069029b869377aa66ec47a42085ff7a
Michele Schio <schio.michele@gmail.com>
2019-11-02 17:27:47 +0100

    maj gantt LAS et gant2latex, tutor pour le BOM et les Devis

--- SU_Suspension/EQ_equipe/csv/gantt2latex.py
+++ SU_Suspension/EQ_equipe/csv/gantt2latex.py
@@ -8,26 +8,29 @@ affichage gantt LAS en partant du fichier csv
 
 import pandas as pd
 
-filename = "Gantt LAS v1"
-taskEnd= 100
+filename = "ganttLAS_v02"
+taskEnd= 125
 ganttDf = pd.read_csv(filename+".csv", header=0, nrows=taskEnd)
 
 def clearNote(text):
     text = str(text)
+    #fix underscore for latex
+    text = text.replace("_", "\\_")
     return text if text!="nan" else ""
 
 def clearTitle(text):
+    # strip indentation spaces
     text = text.replace("  ", "")
+    #fix underscore for latex
+    text = text.replace("_", "\\_")
     return text
 
 
 #%% create task latex
 
 def section(title, note, level=0):
-    # strip the blank spaces
+    # strip the blank spaces and fix underscore
     title = clearTitle(title)
-    # underscore compliance
-    title = title.replace("_", "\\textunderscore ")
     if level >=2:
         latex = "\t"*level+"\\paragraph{{{}}} {}".format(title, note)
     elif level==0:
@@ -50,27 +53,41 @@ with open("tasks.tex", "w") as f:
         
 
 #%% create ressources latex 
-people = set(ganttDf.values[:,5])
+people = set()
+for p in ganttDf.values[:,5]:
+    p = str(p) # for floar nan
+    [people.add(x) for x in p.split(";")]
+
+people.discard("nan")
 
 def person(name, note):
     text = "\\newpage \\section*{{{}}} \n \\par {}".format(name, note)
     return text
 
 
-def task(name, note):
-    text = "\\paragraph{{{}}} {}".format(clearTitle(name), clearNote(note))
+def task(name, note, peopleList = []):
+    if peopleList !=[] :
+        # fromat list to str
+        peopleList = ", ".join(peopleList)
+        text = "\\paragraph{{{}}} \\footnote{{avec {}}} {}".format(clearTitle(name), peopleList,  clearNote(note))
+    else:
+        text = "\\paragraph{{{}}} {}".format(clearTitle(name), clearNote(note))
     return text
 
 with open("team.tex", "w") as f:
     for p in people:
         print(person(p,"personal notes"), file=f)
         for i in range(taskEnd):
-            if p == ganttDf.values[i,5]:
-                taskName = ganttDf.values[i,0]
-                taskNote =  cleanNote(ganttDf.values[i,7])
+            peopleList = ganttDf.values[i,5]
+            taskName = ganttDf.values[i,0]
+            taskNote =  clearNote(ganttDf.values[i,7])
+            if p == peopleList:
+                # task is not shared with others
                 print(task(taskName,taskNote), file=f)
-                
-
-
-
+            elif p in str(peopleList).split(";"):
+                # task is shared with others
+                peopleList = str(peopleList).split(";")
+                # remove the current person
+                peopleList.remove(p)
+                print(task(taskName,taskNote,peopleList) , file=f)
 
