72aadcd9e7841c888873b752e2237232f8670607
ARZ <arthur.rodriguez@aliceadsl.fr>
2020-02-12 22:25:35 +0100

    CC_Cell_Chrono

---
+++ EL_Electrical/Cellule chrono/Main_2/End.h
@@ -0,0 +1,21 @@
+#ifndef Ending
+#define Ending
+
+// Libraries
+#include "Arduino.h"
+#include "SegDisp.h"
+
+// Variables
+
+extern bool Start;
+extern int Run;
+extern bool Finish;
+
+extern unsigned long Instant;
+extern unsigned long Time;
+
+// Functions
+
+void End();
+
+#endif
---
+++ EL_Electrical/Cellule chrono/Main_3/Accel.cpp
@@ -0,0 +1,66 @@
+// Libraries
+#include "Accel.h"
+
+// Variables
+
+// Function
+
+void Accel(){
+
+    Serial.println("Accel");
+
+    Seg_1_Write(1);
+
+    while(Finish!=1){
+
+        if(analogRead(A6)>128){
+            Seg_2_Write_Clear();
+            break;
+        }
+
+        if(Start==0){
+            if(digitalRead(5)==HIGH || digitalRead(6)==HIGH){
+                Serial.println("Detection");
+                delay(100);                
+            } else {
+                if(digitalRead(A0)==HIGH){
+                    Start=1;
+                    Serial.println("Start=1");
+                }
+            }
+        }
+
+        if(Start==1){
+            if(Run==0){
+                if(digitalRead(5)==HIGH || digitalRead(6)==HIGH){
+                    Instant=millis();
+                    Run=1;                
+                    Serial.println("Run=1");
+                    Serial.println(Instant);
+                    Seg_2_Write_Run();
+                    delay(1000);
+                }
+            }
+  
+            if(Run==1){
+                if(digitalRead(5)==HIGH || digitalRead(6)==HIGH){
+                    Time=millis()-Instant;
+                    Finish=1;
+                    Serial.println("Finish=1");
+                    Serial.println(Time);
+                    if(Time<1000000){
+                        Seg_2_Write_Num(Time);
+                        SD_Card_Write(1,Time);
+
+                        delay(5000);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    // On termine le programme
+    End();
+}
