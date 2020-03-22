f383c6267687a50768078e5251f937b711bc6990
Michele Schio <schio.michele@gmail.com>
2020-03-22 19:25:29 +0100

    LapTime lecture script accel

--- MO_Models/LapTime/subfunctions/longitudinal/accelScript.m
+++ MO_Models/LapTime/subfunctions/longitudinal/accelScript.m
@@ -1,33 +1,15 @@
-%% Acceleration Model
-    % Le script suivant permet de simuler une acceleration sur une distance
-    % donnee. De nombreuses ameliorations peuvent etre apportees par la suite.
-    % La courbe de couple moteur doit etre la plus complete possible pour de
-    % meilleurs resultats
-    % Ce script est utile pour voir l'influence des differents parametres sur
-    % le temps de l'acceleration
-%__Inputs :__
-%               - global : Vehicle_file step
-%               - Starting speed (Vi)
-%               - Travel distance (D_acc)
-%__Outputs :___
-%               - vectors of speed, time and distance
-%__Hypotheses__
-    % Le pneu est indeformable
-    % Le coeff d'adherence des pneus est constant ==> modèle MF simplifié
-    % Le coeff de résistance au roulement est constant
-    % Le glissement est considere nul
-    % La voiture n'a pas de suspensions
-    % Le transfert de charge est instantane
-    % Les pertes dans la transmission sont proportionnelles au couple
-    % Le temps de passage de rapport ne depend pas des rapports concernes
-    % Les rapport passent sans debrayer ==> pas du tout vrai ça
-
-%% Paramètres simulation
-function [V_acc,Gx,time] = Accel(D_max,param_file)
-
+%% load params in workspace
+param_file =  '';
 load(param_file)
-step = 0.01; %pas de calcul de la simu
-%__Init__
+
+%pas de calcul de la simu
+step = 0.01; 
+
+% peut etre une putian de bonne idée de découper la dynamique du véhicule
+% du modèle de moteur et de la simulation elle meme
+
+
+%% initialisaton de variables
 dsim = 0; % distance parcourue
 v = 0; % vitesse du vehicule
 tsim = -step; % temps
@@ -42,60 +24,84 @@ E = 1; % Embrayage 1=debraye, 0=embraye
 Adh = 1; % Risque de patinage des pneus
 Ch_ar = rep*m_t*g;
 
-%__preliminar calculs__
+%% calcul preliminaire pour quoi faire ?
+n = t_pas/step; % Nombre de pas necessaire au passage de rapport ==> comment ça ?
 
-n = t_pas/step; % Nombre de pas necessaire au passage de rapport
+% c'est quoi u ??
 u = n;
+
 % Rapport engagé optimal selon la vitesse du véhicule
 k = 6; % Rapport engagé
 for gear=1:5
-    if 0 < shift_Speed(6-gear)
+    if 0 < shift_Speed(6-gear) % ==> vecteur shit_speed??
         k = 6-gear; % Rapport engage
     end
 end
+
+% c'est quoi ça ??
 Ke = 1;
 k = 1;
 
+% what the fuck ??
 b = coeff_roul*2/D_wheel; % Decalage du point d'appui (m)
 c_roul = m_t*g*b; % Resistance au roulement (N.m)
 
+% inerties de rotatin, justement cas la on n'est pas à l'equilibre 
 J_trans = m_t*D_wheel^2/4; % Inertie equivalente des masses en translation (kg.m²)
 J_eq = J_trans + J_rot; % Inertie totale (kg.m²)
-%% Simulation
+
+
+%% Simulation ==> petite description de l'algo non ?
 while dsim < D_max  
+    
+    % intération ==> initialisation pipeau à -step ?!
     tsim = tsim+step;
+    
+    % on rajoute quelque chose
     t_acc = [t_acc tsim];
     Ke = [Ke k]; % Memoire du rapport engage
-    r = v/(k_p*K(k)*k_f*D_wheel*3.14/60); % Calcul du regime moteur
+    r = v/(k_p*K(k)*k_f*D_wheel*pi/60); % Calcul du regime moteur 
+    
     % Prise en compte du patinage de l'embrayage
     if k == 1
         if r < r_pat
             E = [E, 1]; % Patinage
-        else E = [E,0]; % Pas de patinage
+        else
+            E = [E,0]; % Pas de patinage
         end
-        r = max(r,r_pat);
+        r = max(r,r_pat); % ==> le patinage est lié au pneu et pas au regime moteur non ??
     else
         E = [E,0];
     end
     RPM_acc = [RPM_acc r];
+    
     % Acceleration
     if (u < n) || (r > r_rupteur) % Changement de rapport
         a=0; % Acceleration nulle si changement de rapport
         C = [C, 0]; % Memoire couple aux roues
         Gx = [Gx,0]; % Memoire acceleration
         Adh = [Adh, 0]; % Pas de risque de patinage des pneus
-        Ch_ar =[Ch_ar rep*m_t*g];
+        Ch_ar = [Ch_ar rep*m_t*g];
     else     % Pas de changement de rapport 
-        F_deportance_essieu_arr = 1/2*rho*v^2*Cz*Cz_rep;
+        F_deportance_essieu_arr = 1/2*rho*v^2*Cz*Cz_rep; % force aero 
         F_trainee = 1/2*rho*v^2*Cx;
         c_k = interp1(rmot,cmot,r)*pertes/(k_p*K(k)*k_f); % Couple a la roue au rapport engage (m.kg)
         a_ang = (c_k-c_roul)/J_eq; % Acceleration angulaire des roues arrieres
-        C_ar = rep*m_t*g+m_t*a_ang*(D_wheel/2)*h/W + F_deportance_essieu_arr; % Charge sur l'essieu arrière avec prise en compte du transfert de masse
-        if C_ar > m_t*g+ F_deportance_essieu_arr % Cas ou les roues avant se soulevent
+        
+        % Charge sur l'essieu arrière avec prise en compte du transfert de
+        % masse ==> charges.m
+        C_ar = rep*m_t*g+m_t*a_ang*(D_wheel/2)*h/W + F_deportance_essieu_arr; 
+        
+        % Cas ou les roues avant se soulevent
+        if C_ar > m_t*g+ F_deportance_essieu_arr 
             C_ar = m_t*g+ F_deportance_essieu_arr;
         end
-        c_trans_ar = Long_tire_grip*C_ar*D_wheel/2; % Couple maximum transmissible
-        if c_k > c_trans_ar % Risque de patinage des pneus
+        
+        % Couple maximum transmissible ==> modèle de pneu 
+        c_trans_ar = Long_tire_grip*C_ar*D_wheel/2; 
+        
+        % Risque de patinage des pneus
+        if c_k > c_trans_ar
             Adh = [Adh,1];
             c_k = c_trans_ar; % Prise en compte de la limite d'adherence des pneus
             a_ang = (c_k-c_roul)/J_eq; % Acceleration angulaire des roues arrieres
@@ -104,6 +110,8 @@ while dsim < D_max
         else
             Adh = [Adh, 0];
         end
+        
+        
         C = [C, c_k]; % Memoire du couple aux roues
         Ch_ar =[Ch_ar C_ar];
         a = a_ang*D_wheel/2 - F_trainee / (m_t); % Acceleration du vehicule en m/s² non_influence_de_k_f
@@ -140,4 +148,3 @@ idx = setdiff(idx,L);
 V_acc = V_acc(idx);
 Gx = Gx(idx);
 time = tsim;
-end
