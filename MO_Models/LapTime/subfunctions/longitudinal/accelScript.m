1a891471867454f2ae86c6c5c681ee05383df624
Michele Schio <schio.michele@gmail.com>
2020-03-28 20:18:10 +0100

    LapTime maj accel pour GGV

--- MO_Models/LapTime/subfunctions/longitudinal/accelScript.m
+++ MO_Models/LapTime/subfunctions/longitudinal/accelScript.m
@@ -14,17 +14,18 @@ step = 0.01;
 %% initialisaton de variables
 d = 0; % distance parcourue
 v = 0; % vitesse du vehicule
-tsim = 0; % temps
+a = 0; % a_x acceleration longitudinale
+t = 0; % temps de la simulation
 j = 0; % Numero du point de fonctionnement du moteur
-tt = 0; % Temps
-fig_v = figure(2); % Vitesse
-Gx = 0;% Acceleration en g
-fig_r = figure(3); % memoire Regime moteur
+tt = 0; % memoire Temps
+vv = v; % memoire Vitesse
+aa = a; % memoire de a_x --> le mettre en g pour le GGV
+rr = 0; % memoire Regime moteur
 cc = 0; % memoire Couple aux roues 
-dd = 0; % Distance
+dd = d; % memoire Distance
 ee = 1; % Embrayage 1=debraye, 0=embraye
 Adh = 1; % Risque de patinage des pneus
-ZZ = rep*m_t*g;
+ZZ = rep*m_t*g; % memoire charge
 
 % Rapport engagé optimal selon la vitesse du véhicule
 %k = 6; % Rapport engagé
@@ -35,116 +36,96 @@ ZZ = rep*m_t*g;
 %end
 
 k = 1;  % rapport vourant, 1 départ
-fig_k = figure(1);
+kk = k; % memoire rapport engagé
 
-% what the fuck ??
+% what the fuck --> constante paramètres 
 b = coeff_roul*2/D_wheel; % Decalage du point d'appui (m) ==> bras
 c_roul = m_t*g*b; % Resistance au roulement (N.m)
 
-% inerties de rotation, justement cas la on n'est pas à l'equilibre 
+% inerties de rotation, --> const para mètres
 J_trans = m_t*D_wheel^2/4; % Inertie equivalente des masses en translation (kg.m²)
 J_eq = J_trans + J_rot; % Inertie totale (kg.m²)
 
 % Nombre de pas necessaire au passage de rapport ==> comment ça ?
-n = t_pas/step;
-u = n; % c'est quoi u ??
+%n = t_pas/step;
+%u = n; % c'est quoi u ??
 
 
 
-%% Simulation ==> petite description de l'algo non ?
+%% Simulation
 while d < D_max  
-    
-    % on rajoute quelque chose
-    tt = [tt tsim]; % memoire t_acc 
-    figure(fig_k), scatter(tsim, k) % Memoire du rapport engage ==> appena fatto 
-    
-    % Calcul du regime moteur à partir de ls vitesse
-    % ?? on vien de le calculer 
-    r = v/(k_p*K(k)*k_f*D_wheel*pi/60); % rpm
 
     % Prise en compte du patinage de l'embrayage
-    r = min(r,r_pat);
+    r = max(rr(end),r_pat);
+   % rr = [rr r]; % memoire regime moteur
     e = 0 +(k==1)*(r<r_pat);
-    ee = [ee, e];
-    
-    % Prise en compte du patinage de l'embrayage
-    %if k == 1
-    %    if r < r_pat
-    %        ee = [ee, 1]; % Patinage
-    %    else
-%             ee = [ee,0]; % Pas de patinage
-%         end
-%         r = max(r,r_pat);
-%     else
-%         ee = [ee,0];
-%     end
-
-    % update memoire regime moteur 
-    figure(fig_r), scatter(tsim, r);
-    
-    % Acceleration
-    if (u < n) || (r > r_rupteur) % Changement de rapport en cours
-        a_x=0; % Acceleration nulle si changement de rapport
-        cc = [cc, 0]; % Memoire couple aux roues
-        Gx = [Gx,0]; % Memoire acceleration
-        Adh = [Adh, 0]; % Memoire risque de patinage des pneus
-        ZZ = [ZZ rep*m_t*g];
-    else     % Pas de changement de rapport 
+    ee = [ee, e]; % memoire patinage
+   
+%     % Acceleration
+%     if (u < n) || (r > r_rupteur) % Changement de rapport en cours
+%         a_x=0; % Acceleration nulle si changement de rapport
+%         cc = [cc, 0]; % Memoire couple aux roues
+%         Gx = [Gx,0]; % Memoire acceleration
+%         Adh = [Adh, 0]; % Memoire risque de patinage des pneus
+%         ZZ = [ZZ rep*m_t*g];
+%     else     % Pas de changement de rapport 
+%        
+        % Couple a la roue au rapport engage (m.kg)==> moteur
+        c_k = interp1(rmot,cmot,r)*pertes/(k_p*K(k)*k_f); 
+        % Acceleration angulaire des roues arrieres
+        %a_ang = (c_k-c_roul)/J_eq; 
         
-        c_k = interp1(rmot,cmot,r)*pertes/(k_p*K(k)*k_f); % Couple a la roue au rapport engage (m.kg)==> moteur
-        a_ang = (c_k-c_roul)/J_eq; % Acceleration angulaire des roues arrieres
+        % calcule charges avec a_x de la boucle précedente
+        Z = charge(aa(end), v, m_t, rep, h, W ,rho, Cz, Cz_rep);
+        ZZ =[ZZ Z];
         
-        % Charge sur l'essieu arrière avec prise en compte du transfert de masse ==> charges.m
-        Z = charge(a_ang*(D_wheel/2),v, m_t, rep, h, W ,rho, Cz, Cz_rep);
-
         % Couple maximum transmissible ==> modèle de pneu 
         c_amm = Long_tire_grip*Z*D_wheel/2; 
         
-        % Risque de patinage des pneus
-        if c_k > c_amm
-            Adh = [Adh,1]; % Memoire adhérence
-            c_k = c_amm; % Prise en compte de la limite d'adherence des pneus
-            a_ang = (c_k-c_roul)/J_eq; % Acceleration angulaire des roues arrieres
-            Z = charge(a_ang*(D_wheel/2),v, m_t, rep, h, W ,rho, Cz, Cz_rep); % ==> log 
-        else
-            Adh = [Adh, 0];
-        end
-        
-        
+%         % Risque de patinage des pneus
+%         if c_k > c_amm
+%             Adh = [Adh,1]; % Memoire adhérence
+%             c_k = c_amm; % Prise en compte de la limite d'adherence des pneus
+%             a_ang = (c_k-c_roul)/J_eq; % Acceleration angulaire des roues arrieres
+%             Z = charge(a_ang*(D_wheel/2),v, m_t, rep, h, W ,rho, Cz, Cz_rep); % ==> log 
+%         else
+%             Adh = [Adh, 0];
+%         end
+
+        % le pneu peu glisser 
+        c_k = min(c_k, c_amm);
         cc = [cc, c_k]; % Memoire du couple aux roues
-        ZZ =[ZZ Z];
         
-        % PFD - Acceleration du vehicule en m/s² non_influence_de_k_f
+        % maj acceleration angulaire 
+        a_ang = (c_k-c_roul)/J_eq;
+        
+        % PFD - maj a_x
         F_trainee = 1/2*rho*v^2*Cx; % aero
         a_x = a_ang*D_wheel/2 - F_trainee / (m_t); 
-        
-        Gx = [Gx,a_x]; % Memoire acceleration en m/s²
-    end
+        aa = [aa a_x]; % Memoire acceleration en m/s²
+%     end
     
-    v = v + a_x*step; % Vitesse du vehicule
-    figure(fig_v), scatter(tsim, v); % Memoire de la vitesse
-    d = d + v*step; % Distance parcourue par la simulation
+    % maj vitesse du vehicule
+    v = v + a_x*step; 
+    vv = [vv v]; % Memoire de la vitesse
+    
+    % maj distance parcourue par la simulation
+    d = d + v*step; 
     dd = [dd, d]; % Memoire de la distance
+    
+    % maj rapport de vitesse engagé
     r = v/(k_p*K(k)*k_f*D_wheel*pi/60); % Calcul du regime moteur
+    rr = [rr r];
     
-    % Changement de rapport ou changement au rupteur
-    if (r > r_rupteur) && (u >n) && (k<6) && (k<k_max)
-        k = k+1;
-        u=0;
-        % Changement de rapport pour optimiser le couple
-    elseif (k<k_max) && (u >n) && (v*3.6>shift_Speed(k))
-        k = k+1;
-        u=0;        
-    end
-    
-    % a sert à quoi cette variable ???
-    u = u+1;
-    
+    k = k+ (((r > r_rupteur) || (v*3.6>shift_Speed(k)))  && (k<k_max));
+    kk = [kk k]; % Memoire du rapport engage 
+
     % intération temps
-    tsim = tsim+step;
+    t = t+step;
+    tt = [tt t]; % memoire t_acc 
 end
 
-%remove double
+%% remove double
 L = [];
 for i=2:length(vv)
     if vv(i-1)==vv(i)
@@ -155,5 +136,5 @@ end
 idx = 1:length(vv);
 idx = setdiff(idx,L);
 vv = vv(idx);
-Gx = Gx(idx);
-time = tsim;
+aa = aa(idx);
+time = t;
